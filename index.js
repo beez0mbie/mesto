e7a552b8bd8714933e251ad712280a55b2777b04
beez0mbie <vzhik3268@gmail.com>
2023-03-08 14:46:20 +0300

    feat: добавить логику сабмит на форму

--- index.js
+++ index.js
@@ -1,16 +1,43 @@
-const page = document.querySelector(".page");
-const profileEditButton = page.querySelector(".profile__edit-button");
+let page = document.querySelector(".page");
+let profileEditButton = page.querySelector(".profile__edit-button");
 
-const popup = document.querySelector(".popup");
-const closePopupButton = popup.querySelector(".popup__close-button");
+let popup = document.querySelector(".popup");
+let closePopupButton = popup.querySelector(".popup__close-button");
 
-profileEditButton.addEventListener("click", openPopup);
-closePopupButton.addEventListener("click", closePopup);
+let form = popup.querySelector(".popup__form");
+let formInputs = form.querySelectorAll(".popup__input");
+let nameInput = formInputs[0];
+let jobInput = formInputs[1];
 
-function openPopup() {
+function handleOpenPopup() {
+  let profileNameText = page.querySelector(".profile__title").textContent;
+  let profileJobText = page.querySelector(".profile__subtitle").textContent;
+
   popup.classList.add("popup_opened");
+
+  nameInput.value = profileNameText;
+  jobInput.value = profileJobText;
 }
 
-function closePopup() {
+function handleClosePopup() {
   popup.classList.remove("popup_opened");
 }
+
+function handleFormSubmit(evt) {
+  evt.preventDefault();
+
+  let name = nameInput.value;
+  let job = jobInput.value;
+
+  let profileName = page.querySelector(".profile__title");
+  let profileJob = page.querySelector(".profile__subtitle");
+
+  profileName.textContent = name;
+  profileJob.textContent = job;
+
+  handleClosePopup();
+}
+
+profileEditButton.addEventListener("click", handleOpenPopup);
+closePopupButton.addEventListener("click", handleClosePopup);
+form.addEventListener("submit", handleFormSubmit);
