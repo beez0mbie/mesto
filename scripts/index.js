d69d4a02a9981e3ad3c08e08f7abb30d1a01e38e
beez0mbie <vzhik3268@gmail.com>
2023-03-18 17:38:33 +0300

    feat: add function to fill initial cards

--- scripts/index.js
+++ scripts/index.js
@@ -35,3 +35,45 @@ function handleFormSubmit(evt) {
 profileEditButton.addEventListener("click", handleOpenPopup);
 closePopupButton.addEventListener("click", handleClosePopup);
 form.addEventListener("submit", handleFormSubmit);
+
+// Cards
+const fillInitialCards = () => {
+  const initialCards = [
+    {
+      name: "Архыз",
+      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+    },
+    {
+      name: "Челябинская область",
+      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+    },
+    {
+      name: "Иваново",
+      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+    },
+    {
+      name: "Камчатка",
+      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+    },
+    {
+      name: "Холмогорский район",
+      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+    },
+    {
+      name: "Байкал",
+      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+    },
+  ];
+  const cardsElement = page.querySelector(".cards");
+  const templateCard = document.querySelector("#card").content;
+
+  initialCards.forEach((card) => {
+    const cardElement = templateCard.querySelector(".card").cloneNode(true);
+    cardElement.querySelector(".card__image").src = card.link;
+    cardElement.querySelector(".card__image").alt = `Фото: ${card.name}`;
+    cardElement.querySelector(".card__title").textContent = card.name;
+    cardsElement.append(cardElement);
+  });
+};
+
+fillInitialCards();
