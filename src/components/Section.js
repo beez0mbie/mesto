c1327c01ad7c6862f136864955019975c20a5c01
beez0mbie <vzhik3268@gmail.com>
2023-05-12 22:03:48 +0300

    create Section component

---
+++ src/components/Section.js
@@ -0,0 +1,17 @@
+export class Section {
+  constructor({ dataItems, renderer }, containerSelector) {
+    this._itemsToRender = dataItems;
+    this._renderer = renderer;
+    this._container = document.querySelector(containerSelector);
+  }
+
+  addItem(domElement) {
+    this._container.append(domElement);
+  }
+
+  renderItems() {
+    this._itemsToRender.forEach((item) => {
+      this._renderer(item);
+    });
+  }
+}
